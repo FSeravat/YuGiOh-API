d9ed9e19e266f3f644b9da280e99c60e4920a9ba
FSeravat <felipecostatavares@gmail.com>
2023-02-05 03:14:38 -0300

    Progress bar update

--- windows/main_win.py
+++ windows/main_win.py
@@ -32,42 +32,48 @@ class Window:
     def close(self):
         self.window.close()
 
-    def create_deck_list(self, file_list:str) -> list[dict]:
-        card_list:list[dict] = list()
+    def create_deck_list(self, file_list:str) -> list[str]:
+        card_list:list[str] = list()
         file_list:list[str] = file_list.split(";")
+        qtd_files = file_list.__len__()
+        file_count = 1
         for file in file_list:
             txt_deck = open(file, encoding="utf-8").read()
             deck_name = os.path.basename(file)
             id_list:list[int] = re.findall(r'\d{1,9}',txt_deck)
-            layout = [[sg.ProgressBar(id_list.__len__(), orientation='h', size=(30, 20), key='progbar')]]
+            layout = [
+                [sg.Push(), sg.Column([[sg.Text(str(file_count)+"/"+str(qtd_files))]],element_justification='c'), sg.Push()],
+                [sg.ProgressBar(id_list.__len__(), orientation='h', size=(30, 20), key='progbar')]
+            ]
             progress_window = sg.Window("Lendo o deck: "+deck_name,layout)
             event, values = progress_window.read(timeout=False)
             count = 1
-            for card in list(dict.fromkeys(id_list)):
-                card_obj = dict()
-                card_obj["id"] = card
-                card_obj["name"] = api.get_card(card)["name"]
-                card_obj["qtd"] = list(filter(lambda x:x==card,id_list)).__len__()
-                card_list.append(card_obj)
+            progress_window["progbar"].update_bar(count)
+            for card in id_list:
+                card_list.append(api.get_card(card)["name"])
                 progress_window["progbar"].update_bar(count)
                 count+=1
             progress_window.close()
+            file_count+=1
         return card_list
 
-    def create_txt(self, data:list[dict])->None:
+    def create_txt(self, data:list[str])->None:
         txt_data = ""
         qtd = 0
-        three_copys = sorted(list(filter(lambda x:x["qtd"]==3,data)),key=lambda x:x["name"])
-        qtd += three_copys.__len__() * 3
-        two_copys = sorted(list(filter(lambda x:x["qtd"]==2,data)),key=lambda x:x["name"])
-        qtd += two_copys.__len__() * 2
-        one_copys = sorted(list(filter(lambda x:x["qtd"]==1,data)),key=lambda x:x["name"])
-        qtd += one_copys.__len__()
-        all_copys = [three_copys,two_copys,one_copys]
-        for copy in all_copys:
-            for card in copy:
-                txt_data+=card["name"]+" x"+str(card["qtd"])+"\n"
-            txt_data+="\n"
+        card_list:list[dict] = list()
+        for card in list(dict.fromkeys(data)):
+            card_qtd = list(filter(lambda x:x==card,data)).__len__()
+            card_list.append({"name":card,"qtd":card_qtd})
+            qtd+=card_qtd
+        card_list = sorted(card_list,key=lambda x:x["name"])
+        card_list = sorted(card_list,key=lambda x:x["qtd"],reverse=True)
+        actually_qtd = card_list[0]["qtd"]
+        for card in card_list:
+            if actually_qtd != card["qtd"]:
+                actually_qtd = card["qtd"]
+                txt_data+="\n"
+            txt_data+= card["name"]+" x"+str(card["qtd"])+"\n"
+        txt_data+="\n"
         txt_data+="Quantidade de cartas: "+str(qtd)
         with open("resultado.txt",mode="w") as txt_file:
             txt_file.write(txt_data)
